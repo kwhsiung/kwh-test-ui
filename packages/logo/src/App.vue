1791848b13448c5e8e6fcdf7e6c0b7dbe45848ca
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-07-24 13:29:52 +0800

    test change package

--- packages/logo/src/App.vue
+++ packages/logo/src/App.vue
@@ -2,7 +2,7 @@
   <img
     class="logo"
     :src="require(`./assets/readr-logo-${theme}.svg`)"
-    alt="READr logo make some change"
+    alt="READr logo make some change change"
   />
 </template>
 
